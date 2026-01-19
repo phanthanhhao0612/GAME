324003868d1fd60931063dd371f2aaa9f240d4da
phanthanhhao0612 <BIT230145@st.cmc-u.edu.vn>
2026-01-19 19:16:42 +0700

    playermovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+    }
+}
