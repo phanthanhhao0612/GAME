241c2eb5831aac9305a4fcdf7450b0f6ec10b196
phanthanhhao0612 <BIT230145@st.cmc-u.edu.vn>
2026-01-19 19:17:50 +0700

    playermovement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
+        var worldPoint =
+        Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
     }
 }
