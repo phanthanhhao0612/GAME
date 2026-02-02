e8c8a790450a7b497b31e402b5fecb9f85d5c5f8
phanthanhhao0612 <BIT230145@st.cmc-u.edu.vn>
2026-02-02 18:35:52 +0700

    ADD HEALTH POINT TO HEALTH

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public int defaultHealthPoint;
+    private int healthPoint;
 
-    public void OnTriggerEnter2D(Collider2D collision) => Die();
+    private void Start() => healthPoint = defaultHealthPoint;
 
+    public void TakeDamage(int damage)
+    {
+        if (healthPoint <= 0) return;
+
+        healthPoint -= damage;
+        if (healthPoint <= 0) Die();
+    }
     protected virtual void Die()
     {
-        var explosion = Instantiate(explosionPrefab, transform.position,
-        transform.rotation);
+        var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
         Destroy(explosion, 1);
         Destroy(gameObject);
     }
